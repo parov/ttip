41cdf2fefa300824ff5ca365b37a826064d86b39
Parov <pierangelo.turchiarelli@gmail.com>
2015-09-15 21:40:59 +0200

    created rails app

---
+++ ttip/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ttip::Application.config.secret_key_base = 'bf5a46c1f21c87846e490af4b6a5a58145884bdf91c1fc72e3f8a9c12ccf1b79666771856953cf598dbb02d8ac64f1385c874a0055d581b04ada4faba62a405b'
---
+++ ttip/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ttip::Application.config.session_store :cookie_store, key: '_ttip_session'
